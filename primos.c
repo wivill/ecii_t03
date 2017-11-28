b7434e73327fccc06f10658de34f6acffa62683b
Willy <gopwivill@gmail.com>
2017-11-27 20:48:34 -0600

    serial

--- primos.c
+++ primos.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+
+int main(int argc, char const *argv[]) {
+
+  if (argc == 2) {
+    printf("Cantidad de números primos a calcular: %s\n", argv[1]);
+    int cantidad = atoi(argv[1]);
+  }
+  else if (argc > 2) {
+    printf("Se han proprcionado demasiados argumentos.\n");
+  } else {
+    printf("Solo se debe proporcionar como argumento la cantidad de números primos al calcular.\n");
+  }
+
+  return 0;
+}
